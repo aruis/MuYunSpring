a02f755b3a92ae95ae04327c8a9f564f4e192276
Liu Rui <lovearuis@gmail.com>
2026-07-03 10:40:32 +0800

    refactor: 迁移工作流标题配置

--- muyun-boot/src/main/java/net/ximatai/muyun/spring/boot/workflow/WorkflowTitleConfiguration.java
+++ muyun-boot/src/main/java/net/ximatai/muyun/spring/boot/workflow/WorkflowTitleConfiguration.java
@@ -2,16 +2,15 @@ package net.ximatai.muyun.spring.boot.workflow;
 
 import net.ximatai.muyun.spring.iam.user.UserAccountService;
 import net.ximatai.muyun.spring.platform.workflow.WorkflowUserTitleResolver;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import io.quarkus.arc.DefaultBean;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
 
-@Configuration(proxyBeanMethods = false)
+@ApplicationScoped
 public class WorkflowTitleConfiguration {
-    @Bean
-    @ConditionalOnBean(UserAccountService.class)
-    @ConditionalOnMissingBean(WorkflowUserTitleResolver.class)
+    @Produces
+    @ApplicationScoped
+    @DefaultBean
     WorkflowUserTitleResolver workflowUserTitleResolver(UserAccountService userAccountService) {
         return new IamWorkflowUserTitleResolver(userAccountService);
     }
